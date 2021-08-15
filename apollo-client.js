5bc9529bda623db285978f3bb9f4dde39f2d4e79
Wellerson <wellerson.developer@gmail.com>
2021-08-15 20:44:56 -0300

    feat: added apollo client

---
+++ apollo-client.js
@@ -0,0 +1,8 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "https://rocketseat-frontend-challenge.herokuapp.com/",
+    cache: new InMemoryCache(),
+});
+
+export default client;
